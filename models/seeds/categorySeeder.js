17c662be648bbb4e0a082fe3ded240374f2534bf
bmlee <bmlee9527@gmail.com>
2021-04-09 02:38:55 +0800

    feat: modity seed

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -6,14 +6,9 @@ const db = require('../../config/mongoose')
 
 db.once('open', () => {
   console.log('mongodb connected!')
-  for (let i = 0; i < categorySeeder.category.length; i++) {
-    Category.create({
-      category: categorySeeder.category[i].category,
-      categoryIcom: categorySeeder.category[i].categoryIcom,
-    }).then(() => {
+  Category.create(categorySeeder.category)
+    .then(() => {
       return db.close()
     })
-    console.log('done')
-
-  }
+  console.log('done')
 })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -3,18 +3,30 @@ const recordSeeder = require('./record.json')
 
 const db = require('../../config/mongoose')
 
+
+
+// db.once('open', () => {
+//   console.log('mongodb connected!')
+//   for (let i = 0; i < recordSeeder.record.length; i++) {
+//     Record.create({
+//       name: recordSeeder.record[i].name,
+//       category: recordSeeder.record[i].category,
+//       categoryIcom: recordSeeder.record[i].categoryIcom,
+//       date: recordSeeder.record[i].date,
+//       amount: recordSeeder.record[i].amount,
+//     }).then(() => {
+//       return db.close()
+//     })
+//   }
+//   console.log('done')
+// }
+
+
 db.once('open', () => {
   console.log('mongodb connected!')
-  for (let i = 0; i < recordSeeder.record.length; i++) {
-    Record.create({
-      name: recordSeeder.record[i].name,
-      category: recordSeeder.record[i].category,
-      categoryIcom: recordSeeder.record[i].categoryIcom,
-      date: recordSeeder.record[i].date,
-      amount: recordSeeder.record[i].amount,
-    }).then(() => {
+  Record.create(recordSeeder.record)
+    .then(() => {
       return db.close()
     })
-  }
   console.log('done')
 })
