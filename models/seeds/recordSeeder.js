23012120df3b5379e6376e6f93343bc70e8314b7
bmlee <bmlee9527@gmail.com>
2021-06-06 00:23:59 +0800

    modify recordSeeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,32 +1,48 @@
+const bcrypt = require('bcryptjs')
 const Record = require('../record')
-const recordSeeder = require('./record.json')
+const recordSeeder = require('./record.json').record
+const User = require('../user')
 
 const db = require('../../config/mongoose')
 
 
+const SEED_USER = [{
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678',
+},
+{
+  name: 'user2',
+  email: 'user2@example.com',
+  password: '12345678',
+}]
 
-// db.once('open', () => {
-//   console.log('mongodb connected!')
-//   for (let i = 0; i < recordSeeder.record.length; i++) {
-//     Record.create({
-//       name: recordSeeder.record[i].name,
-//       category: recordSeeder.record[i].category,
-//       categoryIcom: recordSeeder.record[i].categoryIcom,
-//       date: recordSeeder.record[i].date,
-//       amount: recordSeeder.record[i].amount,
-//     }).then(() => {
-//       return db.close()
-//     })
-//   }
-//   console.log('done')
-// }
+
 
 
 db.once('open', () => {
   console.log('mongodb connected!')
-  Record.create(recordSeeder.record)
-    .then(() => {
-      return db.close()
-    })
-  console.log('done')
+  SEED_USER.forEach((user) => {
+    bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(user.password, salt))
+      .then(hash => User.create({
+        name: user.name,
+        email: user.email,
+        password: hash
+      }))
+      .then(user => {
+        const userId = user._id
+        return Promise.all(Array.from(
+          { length: recordSeeder.length },
+          (_, i) => Record.create({
+            ...recordSeeder[i], userId
+          })
+        ))
+      })
+      .then(() => {
+        console.log('done')
+        process.exit()
+      })
+  })
 })
